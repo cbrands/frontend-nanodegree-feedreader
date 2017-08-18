452ba9ba1dcc168e7661726391524c36823cb8da
Charles Brands <charles.brands@gmail.com>
2017-08-18 19:31:58 +0200

    ran code through javascript beautifier

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -55,9 +54,9 @@ $(function() {
 
     /* Done: Write a new test suite named "The menu" */
     describe('The menu', function() {
-    
+
         let body = $('body');
-    
+
         /* Done: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -66,29 +65,29 @@ $(function() {
         it('the menu is hidden by default', function() {
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
-        
-         /* Done: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+
+        /* Done: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
         it('clicking the menu icon toggles the visibility of the menu', function() {
             let menuIconLink = $('.menu-icon-link')[0];
-            
+
             //click icon menu should be visible
             menuIconLink.click();
             expect(body.hasClass('menu-hidden')).toBe(false);
-            
+
             //click again and menu should be hidden
             menuIconLink.click();
             expect(body.hasClass('menu-hidden')).toBe(true);
         });
     });
-    
-    
+
+
     /* Done: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        
+
         /* Done: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -98,14 +97,14 @@ $(function() {
         beforeEach(function(done) {
             loadFeed(0, done);
         });
-        
+
         it('there is at least 1 .entry element within the .feed container', function(done) {
-           let entries = $('.feed .entry');
+            let entries = $('.feed .entry');
             expect(entries.length).toBeGreaterThan(0);
             done();
         });
     });
-    
+
     /* Done: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
         /* Done: Write a test that ensures when a new feed is loaded
@@ -113,21 +112,21 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
         let before, after;
-        
+
         //loadFeed store content in before, loadfeed again and store the content in after
         beforeEach(function(done) {
             loadFeed(0, function() {
                 before = $('.feed')[0].innerText;
                 loadFeed(1, function() {
-                   after = $('.feed')[0].innerText;
+                    after = $('.feed')[0].innerText;
                     done();
                 });
             });
         });
-        
+
         //we expect the conent to change so before != after
         it('contents is changed', function(done) {
-           expect(before).not.toBe(after);
+            expect(before).not.toBe(after);
             done();
         });
     });
